86032119d5501a2a48d1bb7341c0c9cfe066c3d2
IvanSoregashi <ivanshiriaev2222@gmail.com>
2026-03-09 14:19:51 +0500

    chore: fix path

--- library/tests/test_xml_document.py
+++ library/tests/test_xml_document.py
@@ -8,7 +8,7 @@ from library.xml.document_custom import XMLDocumentSchema
 from library.xml.document_pydantic import XMLDocumentModel
 from library.xml.utils import get_facts
 
-CONTAINER_PATH = "samples/empty_container.xml"
+CONTAINER_PATH = "tests/samples/empty_container.xml"
 CONTAINER_XML = b'<?xml version="1.0" encoding="utf-8"?><container></container>'
 
 
