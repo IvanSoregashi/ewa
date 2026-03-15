2011e8a516e19ee9051d021857c4364a3b0b5b70
IvanSoregashi <ivanshiriaev2222@gmail.com>
2026-03-15 11:32:47 +0500

    feature: add source to epub

--- library/src/library/epub/epub.py
+++ library/src/library/epub/epub.py
@@ -1,10 +1,20 @@
 import logging
-from os import PathLike
 from pathlib import Path
+from zipfile import is_zipfile
+
+from library.epub.source import DirectorySource, ZipFileSource, SourceProtocol
 
 logger = logging.getLogger(__name__)
 
 
 class EPUB:
-    def __init__(self, path: str | PathLike) -> None:
+    def __init__(self, path: str | Path) -> None:
         self.path = Path(path)
+        self.source: SourceProtocol | None = None
+        if self.path.is_dir():
+            self.source = DirectorySource(path)
+        elif is_zipfile(path):
+            self.source = ZipFileSource(path)
+        if self.source is None:
+            raise FileNotFoundError(f"Source {path} was not recognized as directory or epub(zipfile).")
+
