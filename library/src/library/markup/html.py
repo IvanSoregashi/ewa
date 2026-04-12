5f87ea74c0e6754c73f5baf7d4988d8228ee4574
IvanSoregashi <ivanshiriaev2222@gmail.com>
2026-04-12 20:20:04 +0500

    delete: deprecated translator

--- library/src/library/markup/html.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def translate_text_file(filepath: Path, translation: dict[str, str]):
-    text = filepath.read_text(encoding="utf-8")
-    result = ""
-    for i in text:
-        result += translation.get(i, i)
-    filepath.write_text(result, encoding="utf-8")
