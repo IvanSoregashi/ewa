44dd054590a9722eb9a27a2f329f507907126571
IvanSoregashi <ivanshiriaev2222@gmail.com>
2026-05-13 02:55:53 +0500

    feature: make utils_path.py

---
+++ library/src/library/epub/utils_path.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from epub.utils import string_to_int_hash64
+
+
+def prefix_name_with_hash(path: str | Path, content: bytes | None = None) -> Path:
+    if content is None:
+        if isinstance(path, str) or (isinstance(path, Path) and not path.exists()):
+            raise ValueError("Cannot compute hash with provided data")
+    path: Path = Path(path)
+    content_bytes = content or path.read_bytes()
+    _hash = string_to_int_hash64(content_bytes)
+    return path.with_stem(f"{_hash}_{path.stem}")
