a7fd20f9e76523a25a5eb2d8c2c8370b6dfa9356
IvanSoregashi <ivanshiriaev2222@gmail.com>
2026-05-13 02:50:51 +0500

    feature: update XMLDocumentModel

--- library/src/library/xml/document_pydantic.py
+++ library/src/library/xml/document_pydantic.py
@@ -1,8 +1,12 @@
 from typing import TypeVar
+
+from lxml.etree import Element
 from pydantic_xml import BaseXmlModel
 from library.xml.document import XMLDocument
 
 ModelT = TypeVar("ModelT", bound=BaseXmlModel)
 
 
-class XMLDocumentModel(BaseXmlModel, XMLDocument): ...
+class XMLDocumentModel(BaseXmlModel, XMLDocument):
+    def to_xml_tree(self, skip_empty=True, exclude_none=True, exclude_unset=True) -> Element:
+        return super().to_xml_tree(skip_empty=skip_empty, exclude_none=exclude_none, exclude_unset=exclude_unset)
